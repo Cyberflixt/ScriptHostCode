8d8cd3ca89f5d91bbb7f931faac24d82a5784ad8
Cyberflixt <54700008+Cyberflixt@users.noreply.github.com>
2020-08-18 20:09:26 +0200

    Update jsMain.js

--- jsMain.js
+++ jsMain.js
@@ -3,8 +3,8 @@ var txtScript;
 
 $(document).ready(new function() {
     txtScript = document.getElementById("txtScript");
-    setTimeout(() => {
-        txtScript.innerHTML = "No u."
-    },5)
-    //txtScript.innerHTML = "No u."
+    //setTimeout(() => {
+    //    txtScript.innerHTML = "No u."
+    //},5)
+    txtScript.innerHTML = "No u."
 });
