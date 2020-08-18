f78b0cc511afac5cbeec8a3912a9d54e7006b18c
Cyberflixt <54700008+Cyberflixt@users.noreply.github.com>
2020-08-18 20:07:57 +0200

    Add files via upload

---
+++ jsMain.js
@@ -0,0 +1,10 @@
+
+var txtScript;
+
+$(document).ready(new function() {
+    txtScript = document.getElementById("txtScript");
+    setTimeout(() => {
+        txtScript.innerHTML = "No u."
+    },5)
+    //txtScript.innerHTML = "No u."
+});
